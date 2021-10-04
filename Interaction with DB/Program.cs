2a73038e00d1735c0fad128cad28afca498a0ca1
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-04 17:08:38 +0300

    Добавьте файлы проекта.

---
+++ Interaction with DB/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+
+namespace Interaction_with_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQL.InsertUser();
+            SQL.SelectUsers();
+        }
+
+    }
+
+
+}
---
+++ Interaction with DB/SQL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Interaction_with_DB
+{
+    class SQL
+    {
+        public static void SelectUsers()
+        {
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=db;Integrated Security=True";
+
+                connection.Open();
+
+
+                SqlCommand command = new SqlCommand("select * from users", connection);
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("{0} \t{1} \t{2} ", reader.GetName(0), reader.GetName(1), reader.GetName(2));
+
+                    while (reader.Read())
+                    {
+                        object id = reader.GetValue(0);
+                        object login = reader.GetValue(1);
+                        object role = reader.GetValue(2);
+                        Console.WriteLine("{0} \t{1} \t{2}", id, login, role);
+                    }
+
+                }
+
+
+
+            }
+        }
+
+        public static void InsertUser()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=db;Integrated Security=True";
+
+                connection.Open();
+
+                SqlCommand insert = new SqlCommand("insert into users (id, login, role) values (4, 'larry', 'superadmin')", connection);
+                insert.ExecuteNonQuery();
+
+            }
+        }
+
+    }
+}
